f38cbe8f591e48f0e8bb90d96fa957c804f79f59
Vickie Vien <vickie@DESKTOP-A7CACKI.localdomain>
2022-03-28 18:51:14 -0700

    updated plant model: timestamp added

--- models/plantModel.js
+++ models/plantModel.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const plantSchema = new Schema ({
-    plantName: {type: String, required: true},
     plantType: {type: String, required: true},
+    plantName: String,
     price: {type: Number, required: true},
     img: {type: String, required: true},
-})
+    description: String,
+    email: {type: String, required: true}
+}, timestamp)
 
 const Plant = mongoose.model('Plant', plantSchema);
 
